a6b0e058f49fba4056e8a542a6506b519d1b3622
mseffner <mseffner@gmail.com>
2017-07-16 07:03:27 -0700

    added some networking test code

--- app/src/main/java/mseffner/twitchnotifier/MainActivity.java
+++ app/src/main/java/mseffner/twitchnotifier/MainActivity.java
@@ -1,19 +1,49 @@
 package mseffner.twitchnotifier;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 import mseffner.twitchnotifier.networking.NetworkUtils;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView testTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView test = (TextView) findViewById(R.id.test);
-        test.setText("");
+        testTextView = (TextView) findViewById(R.id.test);
+
+        new TestAsyncTask().execute();
     }
+
+    private class TestAsyncTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            String apiResponse = "";
+
+            try {
+                apiResponse = NetworkUtils.makeHttpRequest();
+            } catch (IOException e) {
+                Log.e("TestAsyncTask", e.toString());
+            }
+
+            return apiResponse;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            testTextView.setText(s);
+        }
+    }
+
 }
