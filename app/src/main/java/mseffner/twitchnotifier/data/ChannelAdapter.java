89722feeeac0dd699a662c29533d81d724750874
mseffner <mseffner@gmail.com>
2017-07-16 14:03:16 -0700

    implemented basic RecyclerView adapter

--- app/src/main/java/mseffner/twitchnotifier/data/ChannelAdapter.java
+++ app/src/main/java/mseffner/twitchnotifier/data/ChannelAdapter.java
@@ -1,30 +1,68 @@
 package mseffner.twitchnotifier.data;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import mseffner.twitchnotifier.R;
 
 
-public class ChannelAdapter extends RecyclerView.Adapter {
+public class ChannelAdapter extends RecyclerView.Adapter<ChannelAdapter.ChannelViewHolder> {
 
-    public static class ChannelViewHolder extends RecyclerView.ViewHolder {
-        public ChannelViewHolder(View itemView) {
-            super(itemView);
-        }
+    private final List<Channel> channelList;
+
+    public ChannelAdapter(List<Channel> channelList) {
+        this.channelList = channelList;
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ChannelViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.follower_list_item, parent, false);
+
+        return new ChannelViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(ChannelViewHolder holder, int position) {
+        holder.bind(position);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return channelList.size();
+    }
+
+    class ChannelViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView channelLogo;
+        private TextView channelName;
+        private TextView currentGame;
+        private TextView offlineText;
+        private TextView streamTitle;
+        private LinearLayout streamInfo;
+
+        ChannelViewHolder(View itemView) {
+            super(itemView);
+            channelLogo = itemView.findViewById(R.id.channel_logo);
+            channelName = itemView.findViewById(R.id.channel_name);
+            currentGame = itemView.findViewById(R.id.current_game);
+            offlineText = itemView.findViewById(R.id.offline_text);
+            streamTitle = itemView.findViewById(R.id.stream_title);
+            streamInfo = itemView.findViewById(R.id.live_stream_info);
+        }
+
+        void bind(int index) {
+            Channel channel = channelList.get(index);
+
+            channelName.setText(channel.getDisplayName());
+        }
     }
 }
