adddb84053c059cfd88ea8ad83ab533666c1a85b
mseffner <mseffner@gmail.com>
2017-07-21 08:25:08 -0700

    made CRUD method private, added public insert The basic CRUD methods will be private helper methods, with public-facing methods for more specific operations

--- app/src/main/java/mseffner/twitchnotifier/data/ChannelDb.java
+++ app/src/main/java/mseffner/twitchnotifier/data/ChannelDb.java
@@ -4,10 +4,16 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+
+import java.io.ByteArrayOutputStream;
+
+import mseffner.twitchnotifier.data.ChannelContract.ChannelEntry;
+
 
 public class ChannelDb {
 
-    private static final String TABLE = ChannelContract.ChannelEntry.TABLE_NAME;
+    private static final String TABLE = ChannelEntry.TABLE_NAME;
     private ChannelDbHelper dbHelper;
 
 
@@ -15,26 +21,57 @@ public class ChannelDb {
         dbHelper = ChannelDbHelper.getInstance(context);
     }
 
-    public Cursor query(String[] projection, String selection, String[] selectionArgs,
+    public boolean insertChannel(Channel channel) {
+
+        ContentValues values = new ContentValues();
+
+        values.put(ChannelEntry._ID, channel.getId());
+        values.put(ChannelEntry.COLUMN_NAME, channel.getName());
+        values.put(ChannelEntry.COLUMN_DISPLAY_NAME, channel.getDisplayName());
+        values.put(ChannelEntry.COLUMN_LOGO_URL, channel.getLogoUrl());
+        values.put(ChannelEntry.COLUMN_CHANNEL_URL, channel.getStreamUrl());
+        values.put(ChannelEntry.COLUMN_PINNED, channel.getPinned());
+        values.put(ChannelEntry.COLUMN_LOGO_BMP, getByteArrayFromBitmap(channel.getLogoBmp()));
+
+        Stream stream = channel.getStream();
+        if (stream != null) {
+            values.put(ChannelEntry.COLUMN_GAME, stream.getCurrentGame());
+            values.put(ChannelEntry.COLUMN_VIEWERS, stream.getCurrentViewers());
+            values.put(ChannelEntry.COLUMN_STATUS, stream.getStatus());
+            values.put(ChannelEntry.COLUMN_STREAM_TYPE, stream.getStreamType());
+            values.put(ChannelEntry.COLUMN_CREATED_AT, stream.getCreatedAt());
+        }
+
+        long numRowsInserted = insert(values);
+        return numRowsInserted == 1;
+    }
+
+    private byte[] getByteArrayFromBitmap(Bitmap bmp) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 0, stream);
+        return stream.toByteArray();
+    }
+
+    private Cursor query(String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
 
         SQLiteDatabase database = dbHelper.getReadableDatabase();
         return database.query(TABLE, projection, selection, selectionArgs, null, null, sortOrder);
     }
 
-    public long insert(ContentValues contentValues) {
+    private long insert(ContentValues contentValues) {
 
         SQLiteDatabase database = dbHelper.getWritableDatabase();
         return database.insert(TABLE, null, contentValues);
     }
 
-    public long update(ContentValues contentValues, String selection, String[] selectionArgs) {
+    private long update(ContentValues contentValues, String selection, String[] selectionArgs) {
 
         SQLiteDatabase database = dbHelper.getWritableDatabase();
         return database.update(TABLE, contentValues, selection, selectionArgs);
     }
 
-    public long delete(String selection, String[] selectionArgs) {
+    private long delete(String selection, String[] selectionArgs) {
 
         SQLiteDatabase database = dbHelper.getWritableDatabase();
         return database.delete(TABLE, selection, selectionArgs);
