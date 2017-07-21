e00f89e4d865303831e5808f7fd73327dadac397
mseffner <mseffner@gmail.com>
2017-07-21 08:25:53 -0700

    streamType now an int, createdAt now a unix timestamp

--- app/src/main/java/mseffner/twitchnotifier/data/Stream.java
+++ app/src/main/java/mseffner/twitchnotifier/data/Stream.java
@@ -1,5 +1,11 @@
 package mseffner.twitchnotifier.data;
 
+import android.annotation.SuppressLint;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 public class Stream {
 
     public static final String STREAM_TYPE_LIVE = "live";
@@ -9,15 +15,26 @@ public class Stream {
     private String currentGame;
     private int currentViewers;
     private String status;
-    private String streamType;
-    private String createdAt;
+    private int streamType;
+    private long createdAt;
 
     public Stream(String currentGame, int currentViewers, String status, String createdAt, String streamType) {
         this.currentGame = currentGame;
         this.currentViewers = currentViewers;
         this.status = status;
-        this.createdAt = createdAt;
-        this.streamType = streamType;
+        this.createdAt = getUnixTimestampFromUTC(createdAt);
+
+        switch (streamType) {
+            case STREAM_TYPE_LIVE:
+                this.streamType = ChannelContract.ChannelEntry.STREAM_TYPE_LIVE;
+                break;
+            case STREAM_TYPE_PLAYLIST:
+                this.streamType = ChannelContract.ChannelEntry.STREAM_TYPE_PLAYLIST;
+                break;
+            case STREAM_TYPE_VODCAST:
+                this.streamType = ChannelContract.ChannelEntry.STREAM_TYPE_VODCAST;
+                break;
+        }
     }
 
     public String getCurrentGame() {
@@ -32,11 +49,22 @@ public class Stream {
         return status;
     }
 
-    public String getCreatedAt() {
+    public long getCreatedAt() {
         return createdAt;
     }
 
-    public String getStreamType() {
+    public int getStreamType() {
         return streamType;
+    }
+
+    private long getUnixTimestampFromUTC(String utcFormattedTimestamp) {
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        format.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return format.parse(utcFormattedTimestamp).getTime() / 1000;
+        } catch (ParseException e) {
+            return 0;
+        }
     }
 }
