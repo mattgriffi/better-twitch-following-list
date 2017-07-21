3e3f5031b680ae74000e8002c2e6cd939be29d73
mseffner <mseffner@gmail.com>
2017-07-20 18:30:43 -0700

    redesigned database to use only 1 table

--- app/src/main/java/mseffner/twitchnotifier/data/ChannelContract.java
+++ app/src/main/java/mseffner/twitchnotifier/data/ChannelContract.java
@@ -9,17 +9,12 @@ public final class ChannelContract {
     public static final String CONTENT_AUTHORITY = "mseffner.twitchnotifier";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
     public static final String PATH_CHANNELS = "channels";
-    public static final String PATH_STREAMS = "streams";
 
     // MIME types for ChannelProvider
     public static final String CONTENT_CHANNELS_LIST_TYPE =
             ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_CHANNELS;
     public static final String CONTENT_CHANNELS_ITEM_TYPE =
             ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_CHANNELS;
-    public static final String CONTENT_STREAMS_LIST_TYPE =
-            ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_STREAMS;
-    public static final String CONTENT_STREAMS_ITEM_TYPE =
-            ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_STREAMS;
 
     private ChannelContract() {}
 
@@ -27,28 +22,24 @@ public final class ChannelContract {
 
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_CHANNELS);
 
+        // General channel data
         public static final String TABLE_NAME = "channels";
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_DISPLAY_NAME = "display_name";
         public static final String COLUMN_LOGO_URL = "logo_url";
         public static final String COLUMN_CHANNEL_URL = "channel_url";
         public static final String COLUMN_LOGO_BMP = "logo_bmp";
-    }
 
-    public static class StreamEntry implements BaseColumns {
-
-        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_STREAMS);
-
-        public static final String TABLE_NAME = "streams";
+        // Stream data
         public static final String COLUMN_GAME = "game";
         public static final String COLUMN_VIEWERS = "viewers";
         public static final String COLUMN_STATUS = "status";
         public static final String COLUMN_STREAM_TYPE = "stream_type";
-        public static final String COLUMN_CREATED_AT = "created_at";
+        public static final String COLUMN_CREATED_AT = "created_at";  // This should be a unix timestamp
 
-        public static final int STREAM_TYPE_LIVE = 0;
-        public static final int STREAM_TYPE_PLAYLIST = 1;
+        public static final int STREAM_TYPE_OFFLINE = 0;
+        public static final int STREAM_TYPE_LIVE = 1;
         public static final int STREAM_TYPE_VODCAST = 2;
-        public static final int STREAM_TYPE_UNKNOWN = 3;
+        public static final int STREAM_TYPE_PLAYLIST = 3;
     }
 }
--- app/src/main/java/mseffner/twitchnotifier/data/ChannelDbHelper.java
+++ app/src/main/java/mseffner/twitchnotifier/data/ChannelDbHelper.java
@@ -5,7 +5,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import mseffner.twitchnotifier.data.ChannelContract.ChannelEntry;
-import mseffner.twitchnotifier.data.ChannelContract.StreamEntry;
 
 
 public class ChannelDbHelper extends SQLiteOpenHelper {
@@ -13,7 +12,7 @@ public class ChannelDbHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "channels.db";
     private static int DATABASE_VERSION = 1;
 
-    private static final String DEFAULT_LOGO_URL = "http://www-cdn.jtvnw.net/images/xarth/404_user_300x300.png";
+    private static final String DEFAULT_LOGO_URL = "https://www-cdn.jtvnw.net/images/xarth/404_user_300x300.png";
 
     public ChannelDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -22,34 +21,24 @@ public class ChannelDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase database) {
 
-
         String CREATE_CHANNELS_TABLE =
-                        "CREATE TABLE " +
-                        ChannelEntry.TABLE_NAME + " (" +
-                        ChannelEntry._ID + " INTEGER PRIMARY KEY, " +
-                        ChannelEntry.COLUMN_NAME + " TEXT NOT NULL, " +
-                        ChannelEntry.COLUMN_DISPLAY_NAME + " TEXT NOT NULL DEFAULT \"unknown_name\", " +
-                        ChannelEntry.COLUMN_CHANNEL_URL + " TEXT NOT NULL, " +
-                        ChannelEntry.COLUMN_LOGO_URL + " TEXT NOT NULL DEFAULT " + DEFAULT_LOGO_URL +  ", " +
-                        ChannelEntry.COLUMN_LOGO_BMP + " BLOB" + ");";
-
-        String CREATE_STREAMS_TABLE =
-                        "CREATE TABLE " +
-                        StreamEntry.TABLE_NAME + " (" +
-                        StreamEntry._ID + " INTEGER PRIMARY KEY, " +
-                        StreamEntry.COLUMN_GAME + " TEXT NOT NULL DEFAULT \"\", " +
-                        StreamEntry.COLUMN_VIEWERS + " INTEGER NOT NULL DEFAULT 0, " +
-                        StreamEntry.COLUMN_STATUS + " TEXT NOT NULL DEFAULT \"\", " +
-                        StreamEntry.COLUMN_STREAM_TYPE + " INTEGER NOT NULL DEFAULT " + StreamEntry.STREAM_TYPE_UNKNOWN + ", " +
-                        StreamEntry.COLUMN_CREATED_AT + " INTEGER NOT NULL DEFAULT 0" + ");";  // Unix timestamp
+            "CREATE TABLE " +
+            ChannelEntry.TABLE_NAME + " (" +
+            ChannelEntry._ID + " INTEGER PRIMARY KEY, " +
+            ChannelEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+            ChannelEntry.COLUMN_DISPLAY_NAME + " TEXT NOT NULL DEFAULT \"unknown_name\", " +
+            ChannelEntry.COLUMN_CHANNEL_URL + " TEXT NOT NULL, " +
+            ChannelEntry.COLUMN_LOGO_URL + " TEXT NOT NULL DEFAULT " + DEFAULT_LOGO_URL +  ", " +
+            ChannelEntry.COLUMN_LOGO_BMP + " BLOB" +
+            ChannelEntry.COLUMN_STREAM_TYPE + " INTEGER NOT NULL DEFAULT " + ChannelEntry.STREAM_TYPE_OFFLINE + ", " +
+            ChannelEntry.COLUMN_STATUS + " TEXT NOT NULL DEFAULT \"\", " +
+            ChannelEntry.COLUMN_GAME + " TEXT NOT NULL DEFAULT \"\", " +
+            ChannelEntry.COLUMN_VIEWERS + " INTEGER NOT NULL DEFAULT 0, " +
+            ChannelEntry.COLUMN_CREATED_AT + " INTEGER NOT NULL DEFAULT 0" + ");";  // Unix timestamp
 
         database.execSQL(CREATE_CHANNELS_TABLE);
-        database.execSQL(CREATE_STREAMS_TABLE);
-
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase database, int i, int i1) {
-        DATABASE_VERSION++;
-    }
+    public void onUpgrade(SQLiteDatabase database, int i, int i1) {}
 }
