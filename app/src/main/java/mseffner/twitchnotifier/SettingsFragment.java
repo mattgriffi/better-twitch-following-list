3f3f8462e40d5db36a63cedc6e388c9ee1c174d4
mseffner <mseffner@gmail.com>
2017-08-27 12:01:50 -0700

    added a couple of settings to the new settings screen Specifically Twitch name and vodcast display settings. Vodcast display setting does not yet do anything.

--- app/src/main/java/mseffner/twitchnotifier/SettingsFragment.java
+++ app/src/main/java/mseffner/twitchnotifier/SettingsFragment.java
@@ -3,7 +3,11 @@ package mseffner.twitchnotifier;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
 
 
 public class SettingsFragment extends PreferenceFragmentCompat implements
@@ -12,11 +16,27 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings);
+
+        // Show the current setting
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        SharedPreferences sharedPreferences = preferenceScreen.getSharedPreferences();
+        int count = preferenceScreen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            Preference preference = preferenceScreen.getPreference(i);
+            if (!(preference instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+            }
+        }
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        // Update the currently displayed setting
+        Preference preference = findPreference(key);
+        if (preference != null && !(preference instanceof CheckBoxPreference)) {
+            setPreferenceSummary(preference, sharedPreferences.getString(key, ""));
+        }
     }
 
     @Override
@@ -29,5 +49,20 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
     public void onStart() {
         super.onStart();
         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    private void setPreferenceSummary(Preference preference, Object value) {
+        String valueString = value.toString();
+        String key = preference.getKey();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int index = listPreference.findIndexOfValue(valueString);
+            if (index >= 0) {
+                preference.setSummary(listPreference.getEntries()[index]);
+            }
+        } else {
+            preference.setSummary(valueString);
+        }
     }
 }
