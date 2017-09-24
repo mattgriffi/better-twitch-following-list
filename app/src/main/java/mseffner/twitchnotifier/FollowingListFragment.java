53381d856b0986eaf169d2714971cf415bbf47a3
mseffner <mseffner@gmail.com>
2017-09-24 10:36:54 -0700

    dummy ListFragments

---
+++ app/src/main/java/mseffner/twitchnotifier/FollowingListFragment.java
@@ -0,0 +1,14 @@
+package mseffner.twitchnotifier;
+
+public class FollowingListFragment extends BaseListFragment {
+
+    @Override
+    protected void initializeData() {
+
+    }
+
+    @Override
+    protected void updateList() {
+
+    }
+}
---
+++ app/src/main/java/mseffner/twitchnotifier/TopListFragment.java
@@ -0,0 +1,15 @@
+package mseffner.twitchnotifier;
+
+public class TopListFragment extends BaseListFragment {
+
+    @Override
+    protected void initializeData() {
+
+    }
+
+    @Override
+    protected void updateList() {
+
+    }
+}
+
