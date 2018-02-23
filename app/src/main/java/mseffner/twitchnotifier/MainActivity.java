defc0de55fc010bdc3ffb55ce09e423935f79e17
mseffner <mseffner@gmail.com>
2018-02-23 01:12:08 -0700

    added dark mode

--- app/src/main/java/mseffner/twitchnotifier/MainActivity.java
+++ app/src/main/java/mseffner/twitchnotifier/MainActivity.java
@@ -1,24 +1,41 @@
 package mseffner.twitchnotifier;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the theme based on whether dark mode is on
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        boolean darkMode = sharedPreferences.getBoolean(getString(R.string.pref_dark_mode), false);
+        setTheme(darkMode ? R.style.AppTheme_Dark : R.style.AppTheme_Light);
+
+        // Set the content view
         setContentView(R.layout.activity_main);
 
+        // Set up ViewPager and TabLayout
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-
         ListPagerAdapter adapter = new ListPagerAdapter(this, getFragmentManager());
-
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.pref_dark_mode))) {
+            // If dark mode has been toggled, recreate activity so new theme is applied
+            recreate();
+        }
     }
 }
--- app/src/main/java/mseffner/twitchnotifier/fragments/SettingsFragment.java
+++ app/src/main/java/mseffner/twitchnotifier/fragments/SettingsFragment.java
@@ -3,12 +3,15 @@ package mseffner.twitchnotifier.fragments;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import mseffner.twitchnotifier.R;
 
@@ -17,28 +20,22 @@ public class SettingsFragment extends PreferenceFragment implements
         SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setSummaries();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
-
-        // Show the current setting
-        PreferenceScreen preferenceScreen = getPreferenceScreen();
-        SharedPreferences sharedPreferences = preferenceScreen.getSharedPreferences();
-        int count = preferenceScreen.getPreferenceCount();
-        for (int i = 0; i < count; i++) {
-            Preference preference = preferenceScreen.getPreference(i);
-            if (!(preference instanceof CheckBoxPreference)) {
-                String value = sharedPreferences.getString(preference.getKey(), "");
-                setPreferenceSummary(preference, value);
-            }
-        }
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         // Update the currently displayed setting
         Preference preference = findPreference(key);
-        if (preference != null && !(preference instanceof CheckBoxPreference)) {
+        if (preference != null && !(preference instanceof SwitchPreference)) {
             setPreferenceSummary(preference, sharedPreferences.getString(key, ""));
         }
     }
@@ -53,6 +50,20 @@ public class SettingsFragment extends PreferenceFragment implements
     public void onStart() {
         super.onStart();
         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    private void setSummaries() {
+        // Show the current setting
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        SharedPreferences sharedPreferences = preferenceScreen.getSharedPreferences();
+        int count = preferenceScreen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            Preference preference = preferenceScreen.getPreference(i);
+            if (!(preference instanceof SwitchPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+            }
+        }
     }
 
     private void setPreferenceSummary(Preference preference, Object value) {
