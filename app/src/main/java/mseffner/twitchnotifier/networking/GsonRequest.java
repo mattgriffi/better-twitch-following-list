f8eb2e0a7a24708284c74976420739905f3d609f
mseffner <mseffner@gmail.com>
2018-02-28 19:56:38 -0700

    created GsonRequest class

---
+++ app/src/main/java/mseffner/twitchnotifier/networking/GsonRequest.java
@@ -0,0 +1,58 @@
+package mseffner.twitchnotifier.networking;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+
+public class GsonRequest<T> extends Request<T> {
+
+    private final Gson gson = new Gson();
+    private final Map<String, String> headers;
+    private Class<T> clazz;
+    private Response.Listener<T> listener;
+
+    public GsonRequest(String url, Class<T> clazz, Map<String, String> headers,
+                       Response.Listener<T> listener, Response.ErrorListener errorListener) {
+        super(Method.GET, url, errorListener);
+        this.headers = headers;
+        this.clazz = clazz;
+        this.listener = listener;
+    }
+
+    @Override
+    protected Response<T> parseNetworkResponse(NetworkResponse response) {
+        try {
+            // Get the json from the response
+            String json = new String(
+                    response.data,
+                    HttpHeaderParser.parseCharset(response.headers));
+            // Build object with gson and return it
+            return Response.success(
+                    gson.fromJson(json, clazz),
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        } catch (JsonSyntaxException e) {
+            return Response.error(new ParseError(e));
+        }
+    }
+
+    @Override
+    protected void deliverResponse(T response) {
+        listener.onResponse(response);
+    }
+
+    @Override
+    public Map<String, String> getHeaders() throws AuthFailureError {
+        return headers != null ? headers : super.getHeaders();
+    }
+}
