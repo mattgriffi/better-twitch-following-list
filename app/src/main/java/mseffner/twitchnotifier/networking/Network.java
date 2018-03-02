01bbd66082d3a7cb7e1fde174e3d36e56776c679
mseffner <mseffner@gmail.com>
2018-03-01 17:11:36 -0700

    added some methods to build url strings

--- app/src/main/java/mseffner/twitchnotifier/networking/Network.java
+++ app/src/main/java/mseffner/twitchnotifier/networking/Network.java
@@ -1,8 +1,13 @@
 package mseffner.twitchnotifier.networking;
 
 
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
 import java.util.HashMap;
 import java.util.Map;
+
+import mseffner.twitchnotifier.settings.SettingsManager;
 
 /**
  * Network provides methods to run asynchronous network operations
@@ -35,9 +40,44 @@ public class Network {
     private static final String PARAM_ID = "id";
     private static final String PARAM_LOGIN = "login";
     private static final String PARAM_TYPE = "type";
-    private static final String PARAM_UsER_ID = "user_id";
+    private static final String PARAM_USER_ID = "user_id";
     private static final String PARAM_USER_LOGIN = "user_login";
 
     private static final String FIRST_MAX = "100";
     private static final String FIRST_DEFAULT = "25";
+
+    /**
+     * @return  url to get the user information for the app user
+     */
+    private static String getUserIdUrl() {
+        String username = SettingsManager.getUsername();
+        return Uri.parse(TWITCH_API_BASE_URL).buildUpon()
+                .appendPath(PATH_USERS)
+                .appendQueryParameter(PARAM_LOGIN, username)
+                .build().toString();
+    }
+
+    /**
+     * @return url to get top 100 streams
+     */
+    private static String getTopStreamsUrl() {
+        return Uri.parse(TWITCH_API_BASE_URL).buildUpon()
+                .appendPath(PATH_STREAMS)
+                .appendQueryParameter(PARAM_FIRST, FIRST_MAX)
+                .build().toString();
+    }
+
+    /**
+     * @return url to get first 100 followed channels after cursor
+     */
+    private static String getFollowsUrl(@Nullable String cursor) {
+        String userId = Long.toString(SettingsManager.getUsernameId());
+        Uri.Builder builder = Uri.parse(TWITCH_API_BASE_URL).buildUpon()
+                .appendEncodedPath(PATH_FOLLOWS)
+                .appendQueryParameter(PARAM_FIRST, FIRST_MAX)
+                .appendQueryParameter(PARAM_FROM_ID, userId);
+        if (cursor != null)
+            builder.appendQueryParameter(PARAM_AFTER, cursor);
+        return builder.toString();
+    }
 }
