8fd1438cef875962e1211b20632d3e7e38e33be4
mseffner <mseffner@gmail.com>
2018-03-03 06:42:59 -0700

    added fields to ChannelEntry

--- app/src/main/java/mseffner/twitchnotifier/data/ChannelContract.java
+++ app/src/main/java/mseffner/twitchnotifier/data/ChannelContract.java
@@ -8,26 +8,34 @@ public final class ChannelContract {
 
     public static class ChannelEntry implements BaseColumns {
 
+        private ChannelEntry() {}
+
         // General channel data
         public static final String TABLE_NAME = "channels";
         public static final String COLUMN_DISPLAY_NAME = "display_name";
+        public static final String COLUMN_LOGIN_NAME = "login_name";
         public static final String COLUMN_LOGO_URL = "logo_url";
         public static final String COLUMN_CHANNEL_URL = "channel_url";
         public static final String COLUMN_PINNED = "pinned";
 
         // Stream data
         public static final String COLUMN_GAME = "game";
+        public static final String COLUMN_GAME_ID = "game_id";
         public static final String COLUMN_VIEWERS = "viewers";
         public static final String COLUMN_STATUS = "status";
         public static final String COLUMN_STREAM_TYPE = "stream_type";
         public static final String COLUMN_CREATED_AT = "created_at";
 
+        // Used to keep track of updates
+        public static final String COLUMN_DIRTY = "dirty";
+
         // Values
         public static final int STREAM_TYPE_OFFLINE = 0;
         public static final int STREAM_TYPE_LIVE = 1;
         public static final int STREAM_TYPE_RERUN = 2;
-        public static final int STREAM_TYPE_PLAYLIST = 3;
         public static final int IS_NOT_PINNED = 0;
         public static final int IS_PINNED = 1;
+        public static final int CLEAN = 0;
+        public static final int DIRTY = 1;
     }
 }
