fe3ec744659b78c2e873d28ac2a49e496f1c4ed9
mseffner <mseffner@gmail.com>
2018-03-04 18:52:04 -0700

    DataUpdateManager can now update follows data, also added multithreading for database operations With the parallel network requests and multithreaded database operations, this is so much faster than the old system it's not even funny.

--- app/src/main/java/mseffner/twitchnotifier/data/DataUpdateManager.java
+++ app/src/main/java/mseffner/twitchnotifier/data/DataUpdateManager.java
@@ -1,14 +1,18 @@
 package mseffner.twitchnotifier.data;
 
 
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.support.annotation.NonNull;
-import android.util.Log;
 
 import com.android.volley.Response;
 
 import java.util.List;
 
+import mseffner.twitchnotifier.networking.Containers;
 import mseffner.twitchnotifier.networking.Requests;
+import mseffner.twitchnotifier.settings.SettingsManager;
 
 /**
  * This class provides methods to update data via asynchronous network
@@ -19,15 +23,82 @@ public class DataUpdateManager {
 
     private static final String TAG = DataUpdateManager.class.getSimpleName();
 
-    private static TopStreamsListener listener;
+    private static TopStreamsListener topStreamsListener;
+    private static FollowsListener followsListener;
+    private static Response.ErrorListener followsErrorListener;
+    private static Handler handler;
 
     public interface TopStreamsListener {
         void onTopStreamsResponse(@NonNull List<Channel> channels);
     }
 
+    public interface FollowsListener {
+        void onFollowsDataUpdated();
+    }
+
+    public static void updateFollowsData(FollowsListener listener,
+                                         final Response.ErrorListener errorListener) {
+        followsListener = listener;
+        followsErrorListener = errorListener;
+
+        // Get all of the follows data in chunks of 100
+        if (!SettingsManager.getUsername().equals(""))
+            Requests.getFollows(null, new FollowsResponseListener(), followsErrorListener);
+    }
+
+    private static class FollowsResponseListener implements Response.Listener<Containers.Follows> {
+        @Override
+        public void onResponse(Containers.Follows followsResponse) {
+            if (followsListener == null) {
+                // TODO cancel update, clean database
+            }
+
+            // HandlerThread for database operations
+            if (handler == null) {
+                HandlerThread handlerThread = new HandlerThread("DatabaseOperations");
+                handlerThread.start();
+                Looper looper = handlerThread.getLooper();
+                handler = new Handler(looper);
+            }
+
+            handler.post(() -> ChannelDb.insertFollowsData(followsResponse));
+
+            ContainerParser parser = new ContainerParser();
+            parser.setFollows(followsResponse);
+            long[] userIds = parser.getUserIdsFromFollows();
+
+            // Get users data
+            Requests.getUsers(userIds, usersResponse -> handler.post(() -> ChannelDb.updateUsersData(usersResponse)),
+                    followsErrorListener);
+
+            // Get streams data
+            Requests.getStreams(userIds, streamsResponse -> {
+                handler.post(() -> ChannelDb.updateStreamsData(streamsResponse));
+
+                // Get the games data
+                ContainerParser gamesParser = new ContainerParser();
+                gamesParser.setStreams(streamsResponse);
+                Requests.getGames(gamesParser.getGameIdsFromStreams(),
+                        gamesResponse -> handler.post(() -> ChannelDb.updateGamesData(gamesResponse)),
+                        followsErrorListener);
+
+            }, followsErrorListener);
+
+            // If there are 100 elements, then there might be more to fetch
+            if (parser.getFollowsDataSize() >= 100) {
+                String cursor = parser.getFollowsCursor();
+                // Recursively fetch more data
+                Requests.getFollows(cursor, new FollowsResponseListener(), followsErrorListener);
+            } else {
+                // We are done
+                followsListener.onFollowsDataUpdated();
+            }
+        }
+    }
+
     public static void getTopStreamsData(@NonNull TopStreamsListener listener,
                                          final Response.ErrorListener errorListener) {
-        DataUpdateManager.listener = listener;
+        DataUpdateManager.topStreamsListener = listener;
         ContainerParser parser = new ContainerParser();
 
         // Get the top streams
@@ -49,9 +120,8 @@ public class DataUpdateManager {
     }
 
     private static synchronized void notifyListener(ContainerParser parser) {
-        Log.e(TAG, "notifyListener");
-        if (parser == null || !parser.isDataComplete() || listener == null) return;
-        listener.onTopStreamsResponse(parser.getChannelList());
-        listener = null;
+        if (parser == null || !parser.isDataComplete() || topStreamsListener == null) return;
+        topStreamsListener.onTopStreamsResponse(parser.getChannelList());
+        topStreamsListener = null;
     }
 }
