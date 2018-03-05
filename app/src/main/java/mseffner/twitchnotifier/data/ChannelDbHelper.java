dc7b329e6d7f95f6d7fdd9f16b9bbdf285639df7
mseffner <mseffner@gmail.com>
2018-03-04 20:04:17 -0700

    added CREATE statements for new tables

--- app/src/main/java/mseffner/twitchnotifier/data/ChannelDbHelper.java
+++ app/src/main/java/mseffner/twitchnotifier/data/ChannelDbHelper.java
@@ -5,6 +5,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import mseffner.twitchnotifier.data.ChannelContract.ChannelEntry;
+import mseffner.twitchnotifier.data.ChannelContract.FollowEntry;
+import mseffner.twitchnotifier.data.ChannelContract.GameEntry;
+import mseffner.twitchnotifier.data.ChannelContract.UserEntry;
+import mseffner.twitchnotifier.data.ChannelContract.StreamEntry;
 
 public class ChannelDbHelper extends SQLiteOpenHelper {
 
@@ -19,7 +23,8 @@ public class ChannelDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase database) {
-        database.execSQL(getCreateStatement(ChannelEntry.TABLE_NAME));
+        // TODO create version 3 tables
+        database.execSQL(getVersion2CreateStatement(ChannelEntry.TABLE_NAME));
     }
 
     @Override
@@ -29,6 +34,8 @@ public class ChannelDbHelper extends SQLiteOpenHelper {
             switch (oldVersion) {
                 case 1:
                     upgradeFrom1to2(database);
+                case 2:
+                    upgradeFrom2to3(database);
             }
             database.setTransactionSuccessful();
         } finally {
@@ -36,7 +43,53 @@ public class ChannelDbHelper extends SQLiteOpenHelper {
         }
     }
 
-    private static String getCreateStatement(String tableName) {
+    private static String getFollowsCreateStatement(String tableName) {
+        return "CREATE TABLE " + tableName +
+            " (" +
+                FollowEntry._ID + " INTEGER PRIMARY KEY, " +
+                FollowEntry.COLUMN_PINNED + " INTEGER NOT NULL DEFAULT " + FollowEntry.IS_NOT_PINNED + ", " +
+                FollowEntry.COLUMN_DIRTY + " INTEGER NOT NULL DEFAULT " + FollowEntry.CLEAN +
+            ");";
+    }
+
+    private static String getUsersCreateStatement(String tableName) {
+        return "CREATE TABLE " + tableName +
+            " (" +
+                UserEntry._ID + " INTEGER PRIMARY KEY, " +
+                UserEntry.COLUMN_LOGIN + " TEXT NOT NULL DEFAULT \"\", " +
+                UserEntry.COLUMN_DISPLAY_NAME + " TEXT NOT NULL DEFAULT \"\", " +
+                UserEntry.COLUMN_PROFILE_IMAGE_URL + " TEXT NOT NULL DEFAULT \"\"" +
+            ");";
+    }
+
+    private static String getStreamsCreateStatement(String tableName) {
+        return "CREATE TABLE " + tableName +
+            " (" +
+                StreamEntry._ID + " INTEGER PRIMARY KEY, " +
+                StreamEntry.COLUMN_GAME_ID + " INTEGER NOT NULL DEFAULT 0, " +
+                StreamEntry.COLUMN_TYPE + " TEXT NOT NULL DEFAULT " + StreamEntry.STREAM_TYPE_OFFLINE + ", " +
+                StreamEntry.COLUMN_TITLE + " TEXT NOT NULL DEFAULT \"\"" +
+                StreamEntry.COLUMN_VIEWER_COUNT + " INTEGER NOT NULL DEFAULT 0" +
+                StreamEntry.COLUMN_STARTED_AT + " INTEGER NOT NULL DEFAULT 0" +
+                StreamEntry.COLUMN_LANGUAGE + " TEXT NOT NULL DEFAULT \"en\"" +
+                StreamEntry.COLUMN_THUMBNAIL_URL + " TEXT NOT NULL DEFAULT \"\"" +
+            ");";
+    }
+
+    private static String getGamesCreateStatement(String tableName) {
+        return "CREATE TABLE " + tableName +
+            " (" +
+                GameEntry._ID + " INTEGER PRIMARY KEY, " +
+                GameEntry.COLUMN_NAME + " TEXT NOT NULL DEFAULT \"\"" +
+                GameEntry.COLUMN_BOX_ART_URL + " TEXT NOT NULL DEFAULT \"\"" +
+            ");";
+    }
+
+    private static void createVersion3(SQLiteDatabase database) {
+        // TODO implement
+    }
+
+    private static String getVersion2CreateStatement(String tableName) {
         return "CREATE TABLE " + tableName +
             " (" +
                 // The ID will be the ID returned by the Twitch API, which allows me to use it
@@ -59,7 +112,7 @@ public class ChannelDbHelper extends SQLiteOpenHelper {
     }
 
     private static void upgradeFrom1to2(SQLiteDatabase database) {
-        String create = getCreateStatement("temp");
+        String create = getVersion2CreateStatement("temp");
         String columnList = ChannelEntry._ID + ", " + ChannelEntry.COLUMN_DISPLAY_NAME + ", " +
                 ChannelEntry.COLUMN_CHANNEL_URL + ", " + ChannelEntry.COLUMN_LOGO_URL + ", " +
                 ChannelEntry.COLUMN_PINNED + ", " + ChannelEntry.COLUMN_STREAM_TYPE + ", " +
@@ -78,5 +131,9 @@ public class ChannelDbHelper extends SQLiteOpenHelper {
         database.execSQL(drop);
         // Rename temp table
         database.execSQL(rename);
+    }
+
+    private static void upgradeFrom2to3(SQLiteDatabase database) {
+        // TODO implement
     }
 }
