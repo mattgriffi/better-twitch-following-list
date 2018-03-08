50a095d726e36e8b9cc0558c8b7ebc571d83b3dc
mseffner <mseffner@gmail.com>
2018-03-07 18:23:48 -0700

    added listeners to db insert methods, removed unused methods

--- app/src/main/java/mseffner/twitchnotifier/data/ChannelDb.java
+++ app/src/main/java/mseffner/twitchnotifier/data/ChannelDb.java
@@ -7,13 +7,12 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.TimeZone;
 
 import mseffner.twitchnotifier.data.ChannelContract.FollowEntry;
@@ -25,29 +24,36 @@ import mseffner.twitchnotifier.networking.Containers;
 
 public class ChannelDb {
 
-    private static ChannelDbHelper dbHelper;
+    public interface InsertListener {
+        void onInsertFinished();
+    }
 
+    private static ChannelDbHelper dbHelper;
+    private static boolean notifyListeners;
 
     private ChannelDb() {
     }
 
     public static void initialize(Context context) {
         dbHelper = new ChannelDbHelper(context.getApplicationContext());
+        notifyListeners = true;
     }
 
     public static void destroy() {
         dbHelper = null;
+        notifyListeners = false;
     }
 
-    public static void insertFollowsData(@NonNull Containers.Follows follows) {
+    public static void insertFollowsData(@NonNull Containers.Follows follows, @Nullable InsertListener listener) {
         for (Containers.Follows.Data data : follows.data) {
             ContentValues values = new ContentValues();
             values.put(FollowEntry._ID, Long.parseLong(data.to_id));
             insert(FollowEntry.TABLE_NAME, values);
         }
+        notifyListener(listener);
     }
 
-    public static void insertGamesData(@NonNull Containers.Games games) {
+    public static void insertGamesData(@NonNull Containers.Games games, @Nullable InsertListener listener) {
         for (Containers.Games.Data data : games.data) {
             ContentValues values = new ContentValues();
             values.put(GameEntry._ID, Long.parseLong(data.id));
@@ -55,9 +61,10 @@ public class ChannelDb {
             values.put(GameEntry.COLUMN_BOX_ART_URL, data.box_art_url);
             insert(GameEntry.TABLE_NAME, values);
         }
+        notifyListener(listener);
     }
 
-    public static void insertUsersData(@NonNull Containers.Users users) {
+    public static void insertUsersData(@NonNull Containers.Users users, @Nullable InsertListener listener) {
         for (Containers.Users.Data data : users.data) {
             ContentValues values = new ContentValues();
             values.put(UserEntry._ID, Long.parseLong(data.id));
@@ -66,9 +73,10 @@ public class ChannelDb {
             values.put(UserEntry.COLUMN_PROFILE_IMAGE_URL, data.profile_image_url);
             insert(UserEntry.TABLE_NAME, values);
         }
+        notifyListener(listener);
     }
 
-    public static void insertStreamsData(@NonNull Containers.Streams streams) {
+    public static void insertStreamsData(@NonNull Containers.Streams streams, @Nullable InsertListener listener) {
         for (Containers.Streams.Data data : streams.data) {
             ContentValues values = new ContentValues();
             values.put(StreamEntry._ID, Long.parseLong(data.user_id));
@@ -84,52 +92,12 @@ public class ChannelDb {
 
             insert(StreamEntry.TABLE_NAME, values);
         }
+        notifyListener(listener);
     }
 
-    public static void updateUsersData(@NonNull Containers.Users users) {
-        for (Containers.Users.Data data : users.data) {
-            ContentValues values = new ContentValues();
-            values.put(UserEntry.COLUMN_LOGIN, data.login);
-            values.put(UserEntry.COLUMN_DISPLAY_NAME, data.display_name);
-            values.put(UserEntry.COLUMN_PROFILE_IMAGE_URL, data.profile_image_url);
-
-            String selection = UserEntry._ID + "=?";
-            String[] selectionArgs = {data.id};
-
-            update(UserEntry.TABLE_NAME, values, selection, selectionArgs);
-        }
-    }
-
-    public static void updateStreamsData(@NonNull Containers.Streams streams) {
-        for (Containers.Streams.Data data : streams.data) {
-            ContentValues values = new ContentValues();
-            values.put(StreamEntry.COLUMN_GAME_ID, Long.parseLong(data.game_id));
-            int streamType = data.type.equals("live") ? StreamEntry.STREAM_TYPE_LIVE : StreamEntry.STREAM_TYPE_RERUN;
-            values.put(StreamEntry.COLUMN_TYPE, streamType);
-            values.put(StreamEntry.COLUMN_TITLE, data.title);
-            values.put(StreamEntry.COLUMN_VIEWER_COUNT, data.viewer_count);
-            values.put(StreamEntry.COLUMN_STARTED_AT, getUnixTimestampFromUTC(data.started_at));
-            values.put(StreamEntry.COLUMN_LANGUAGE, data.language);
-            values.put(StreamEntry.COLUMN_THUMBNAIL_URL, data.thumbnail_url);
-
-            String selection = StreamEntry._ID + "=?";
-            String[] selectionArgs = {data.user_id};
-
-            update(StreamEntry.TABLE_NAME, values, selection, selectionArgs);
-        }
-    }
-
-    public static void updateGamesData(@NonNull Containers.Games games) {
-        for (Containers.Games.Data data : games.data) {
-            ContentValues values = new ContentValues();
-            values.put(GameEntry.COLUMN_NAME, data.name);
-            values.put(GameEntry.COLUMN_BOX_ART_URL, data.box_art_url);
-
-            String selection = GameEntry._ID + "=?";
-            String[] selectionArgs = {data.id};
-
-            update(GameEntry.TABLE_NAME, values, selection, selectionArgs);
-        }
+    private static void notifyListener(InsertListener listener) {
+        if (notifyListeners && listener != null)
+            listener.onInsertFinished();
     }
 
     public static List<ListEntry> getAllChannels() {
@@ -183,44 +151,6 @@ public class ChannelDb {
         }
         cursor.close();
         return list;
-    }
-
-    public static Set<Long> getChannelIdSet() {
-        Cursor cursor = query(FollowEntry.TABLE_NAME, true, new String[]{FollowEntry._ID}, null, null, null);
-        Set<Long> idSet = new HashSet<>();
-        int i = cursor.getColumnIndex(FollowEntry._ID);
-        while (cursor.moveToNext())
-            idSet.add(cursor.getLong(i));
-        return idSet;
-    }
-
-    public static Set<Long> getUserIdSet() {
-        Cursor cursor = query(UserEntry.TABLE_NAME, true, new String[]{UserEntry._ID}, null, null, null);
-        Set<Long> idSet = new HashSet<>();
-        int i = cursor.getColumnIndex(UserEntry._ID);
-        while (cursor.moveToNext())
-            idSet.add(cursor.getLong(i));
-        return idSet;
-    }
-
-    public static Set<Long> getGameIdSet() {
-        Cursor cursor = query(GameEntry.TABLE_NAME, true, new String[]{GameEntry._ID}, null, null, null);
-        Set<Long> idSet = new HashSet<>();
-        int i = cursor.getColumnIndex(GameEntry._ID);
-        while (cursor.moveToNext())
-            idSet.add(cursor.getLong(i));
-        return idSet;
-    }
-
-    public static long[] getAllFollowIds() {
-        Cursor cursor = query(FollowEntry.TABLE_NAME, new String[]{FollowEntry._ID}, null, null, null);
-        long[] idArray = new long[cursor.getCount()];
-        int idColumnIndex = cursor.getColumnIndex(FollowEntry._ID);
-
-        int i = 0;
-        while (cursor.moveToNext())
-            idArray[i++] = cursor.getLong(idColumnIndex);
-        return idArray;
     }
 
     public static long[] getUnknownUserIds() {
@@ -304,13 +234,6 @@ public class ChannelDb {
                                 String sortOrder) {
         SQLiteDatabase database = dbHelper.getReadableDatabase();
         return database.query(tableName, projection, selection, selectionArgs, null, null, sortOrder);
-    }
-
-    private static Cursor query(String tableName, boolean distinct, String[] projection,
-                                String selection, String[] selectionArgs, String sortOrder) {
-        SQLiteDatabase database = dbHelper.getReadableDatabase();
-        return database.query(distinct, tableName, projection, selection,
-                selectionArgs, null, null, sortOrder, null);
     }
 
     private static void insert(String tableName, ContentValues contentValues) {
