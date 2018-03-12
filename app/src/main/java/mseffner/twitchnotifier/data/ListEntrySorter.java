f23a21648e5a82c9f2c8ed13f564021d336a444b
mseffner <mseffner@gmail.com>
2018-03-11 17:01:37 -0700

    ListEntry.gameName is now never null

--- app/src/main/java/mseffner/twitchnotifier/data/ListEntrySorter.java
+++ app/src/main/java/mseffner/twitchnotifier/data/ListEntrySorter.java
@@ -13,8 +13,41 @@ public class ListEntrySorter {
     public static void sort(List<ListEntry> list) {
         Collections.sort(list, new DisplayNameLexicographicComparator());
         Collections.sort(list, new ViewerCountComparator());
+        doFinalSort(list);
         Collections.sort(list, new PinnedComparator());
         Collections.sort(list, new TypeComparator());
+    }
+
+    private static void doFinalSort(List<ListEntry> list) {
+        int sortBy = SettingsManager.getSortBySetting();
+        boolean ascending = SettingsManager.getSortAscendingSetting();
+
+        switch (sortBy) {
+            case SettingsManager.SORT_BY_VIEWER_COUNT:
+                if (ascending)
+                    Collections.sort(list, Collections.reverseOrder(new ViewerCountComparator()));
+                else
+                    Collections.sort(list, new ViewerCountComparator());
+                break;
+            case SettingsManager.SORT_BY_NAME:
+                if (ascending)
+                    Collections.sort(list, new DisplayNameLexicographicComparator());
+                else
+                    Collections.sort(list, Collections.reverseOrder(new DisplayNameLexicographicComparator()));
+                break;
+            case SettingsManager.SORT_BY_GAME:
+                if (ascending)
+                    Collections.sort(list, new GameNameLexicographicComparator());
+                else
+                    Collections.sort(list, Collections.reverseOrder(new GameNameLexicographicComparator()));
+                break;
+            case SettingsManager.SORT_BY_UPTIME:
+                if (ascending)
+                    Collections.sort(list, Collections.reverseOrder(new UptimeComparator()));
+                else
+                    Collections.sort(list, new UptimeComparator());
+                break;
+        }
     }
 
     private static class TypeComparator implements Comparator<ListEntry> {
@@ -53,6 +86,24 @@ public class ListEntrySorter {
         @Override
         public int compare(ListEntry a, ListEntry b) {
             return a.displayName.compareToIgnoreCase(b.displayName);
+        }
+    }
+
+    private static class GameNameLexicographicComparator implements Comparator<ListEntry> {
+        @Override
+        public int compare(ListEntry a, ListEntry b) {
+            return a.gameName.compareToIgnoreCase(b.gameName);
+        }
+    }
+
+    private static class UptimeComparator implements  Comparator<ListEntry> {
+        @Override
+        public int compare(ListEntry a, ListEntry b) {
+            if (b.startedAt - a.startedAt > 0)
+                return 1;
+            else if (b.startedAt - a.startedAt < 0)
+                return -1;
+            return 0;
         }
     }
 }
