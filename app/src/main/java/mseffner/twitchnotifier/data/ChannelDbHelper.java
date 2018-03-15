983d9ee92cb2d8bf408ffb9cf93515791167fb96
mseffner <mseffner@gmail.com>
2018-03-14 19:43:36 -0700

    changed version numbers

--- app/src/main/java/mseffner/twitchnotifier/data/ChannelDbHelper.java
+++ app/src/main/java/mseffner/twitchnotifier/data/ChannelDbHelper.java
@@ -7,7 +7,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 public class ChannelDbHelper extends SQLiteAssetHelper {
 
     private static final String DATABASE_NAME = "channels.db";
-    private static int DATABASE_VERSION = 3;
+    private static int DATABASE_VERSION = 2;
 
     public ChannelDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
