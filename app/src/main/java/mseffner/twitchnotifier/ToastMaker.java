9903d47ec04a812a3870e937b0348b87e5d047c5
mseffner <mseffner@gmail.com>
2018-03-15 17:35:48 -0700

    moved toast messages to strings.xml

--- app/src/main/java/mseffner/twitchnotifier/ToastMaker.java
+++ app/src/main/java/mseffner/twitchnotifier/ToastMaker.java
@@ -2,6 +2,7 @@ package mseffner.twitchnotifier;
 
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.widget.Toast;
 
 /**
@@ -9,17 +10,23 @@ import android.widget.Toast;
  */
 public class ToastMaker {
 
-    public static final String MESSAGE_INVALID_USERNAME = "Invalid username";
-    public static final String MESSAGE_NETWORK_ERROR = "A network error has occurred";
-    public static final String MESSAGE_SERVER_ERROR = "A Twitch server error has occurred";
-    public static final String MESSAGE_TOO_MANY_FOLLOWS = "Cannot have more than 2500 follows due to Twitch API limits";
-    public static final String MESSAGE_RATE_LIMIT = "Twitch API limit hit, data may be incomplete for now";
+    public static String MESSAGE_INVALID_USERNAME;
+    public static String MESSAGE_NETWORK_ERROR;
+    public static String MESSAGE_SERVER_ERROR;
+    public static String MESSAGE_TOO_MANY_FOLLOWS;
+    public static String MESSAGE_RATE_LIMIT;
 
     private static Context appContext;
 
     public static void initialize(Context context) {
         // It must be application context in order to use default Toast style
         appContext = context.getApplicationContext();
+        Resources res = appContext.getResources();
+        MESSAGE_INVALID_USERNAME = res.getString(R.string.message_invalid_username);
+        MESSAGE_NETWORK_ERROR = res.getString(R.string.message_network_error);
+        MESSAGE_SERVER_ERROR = res.getString(R.string.message_server_error);
+        MESSAGE_TOO_MANY_FOLLOWS = res.getString(R.string.message_too_many_follows);
+        MESSAGE_RATE_LIMIT = res.getString(R.string.message_rate_limit);
     }
 
     public static void destroy() {
